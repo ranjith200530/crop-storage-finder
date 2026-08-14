1fb606f2c2ddd23681df6095afbb61bbf98ee901
ranjith200530 <ranjith8@gmail.com>
2026-08-15 02:43:12 +0530

    add the feature of converting address to latitude and longitude

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,3 +1,129 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from accounts.models import SubDistrict,State,District
+
+
+class Storage(models.Model):
+
+    # =========================
+    # STORAGE INFORMATION
+    # =========================
+
+    storage_name = models.CharField(
+        max_length=200
+    )
+
+    storage_type = models.CharField(
+        max_length=50,
+        choices=[
+            ("cold_storage", "Cold Storage"),
+            ("warehouse", "Warehouse"),
+        ]
+    )
+
+    # =========================
+    # CAPACITY
+    # =========================
+
+    capacity = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    capacity_unit = models.CharField(
+        max_length=20,
+        choices=[
+            ("kg", "Kg"),
+            ("quintal", "Quintal"),
+            ("ton", "Ton"),
+        ]
+    )
+
+    # =========================
+    # STORAGE CHARGE
+    # =========================
+
+    # Storage charge is ₹ per kg per month
+    price_per_kg = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    # =========================
+    # LOCATION
+    # =========================
+
+    # SubDistrict already points to:
+    # SubDistrict → District → State
+
+    state = models.ForeignKey(
+            State,
+            on_delete=models.PROTECT
+        )
+    
+    district = models.ForeignKey(
+            District,
+            on_delete=models.PROTECT
+        )
+    
+    subdistrict = models.ForeignKey(
+            SubDistrict,
+            on_delete=models.PROTECT
+        )
+    
+
+    # Complete local address
+    address = models.TextField()
+
+    # =========================
+    # COORDINATES
+    # =========================
+
+    latitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        null=True,
+        blank=True
+    )
+
+    longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        null=True,
+        blank=True
+    )
+
+    # =========================
+    # CONTACT INFORMATION
+    # =========================
+
+    main_contact_number = models.CharField(
+        max_length=15
+    )
+
+    additional_contact_number = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True
+    )
+
+    # =========================
+    # TIMESTAMPS
+    # =========================
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    # =========================
+    # DISPLAY
+    # =========================
+
+    def __str__(self):
+        return self.storage_name
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,7 +1,135 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Storage
+from accounts.models import State, District, SubDistrict
+import requests
+from django.conf import settings
+
 
 # Create your views here.
 def home(request):
     return render(request,"admin_templates/admin_dashboard.html")
+
+
+
+
+
 def register_storage(request):
-    return render(request,"admin_templates/storage_register.html")
+    states=State.objects.all()
+    if request.method == "POST":
+
+        storage_name = request.POST.get("storage_name")
+        storage_type = request.POST.get("storage_type")
+
+        capacity = request.POST.get("capacity")
+        capacity_unit = request.POST.get("capacity_unit")
+
+        price_per_kg = request.POST.get("price_per_kg")
+
+        state_id = request.POST.get("state")
+        district_id = request.POST.get("district")
+        subdistrict_id = request.POST.get("sub_district")
+
+        address = request.POST.get("address")
+
+        main_contact_number = request.POST.get(
+            "main_contact_number"
+        )
+
+        additional_contact_number = request.POST.get(
+            "additional_contact_number"
+        )
+
+
+        # -----------------------------------
+        # GET STATE, DISTRICT, SUBDISTRICT
+        # -----------------------------------
+
+        state = get_object_or_404(
+            State,
+            id=state_id
+        )
+
+        district = get_object_or_404(
+            District,
+            id=district_id
+        )
+
+        subdistrict = get_object_or_404(
+            SubDistrict,
+            id=subdistrict_id
+        )
+        full_address = f"{address},{subdistrict.name}, {district.name}, {state.name}, India"
+
+
+        api_key = settings.GOOGLE_GEOCODING_API_KEY
+
+        url = "https://geocode.googleapis.com/v4/geocode/address"
+        params = {
+            "addressQuery": full_address,
+            "key": api_key
+        }
+
+        response = requests.get(
+            url,
+            params=params
+        )
+        
+        data = response.json()
+        
+
+        # -----------------------------------
+        # GET LATITUDE AND LONGITUDE
+        # -----------------------------------
+
+        latitude = None
+        longitude = None
+
+        if data:
+
+            location = data["results"][0]["location"]
+
+            latitude = location["latitude"]
+            longitude = location["longitude"]
+
+
+        # -----------------------------------
+        # SAVE STORAGE
+        # -----------------------------------
+
+        Storage.objects.create(
+
+            storage_name=storage_name,
+
+            storage_type=storage_type,
+
+            capacity=capacity,
+
+            capacity_unit=capacity_unit,
+
+            price_per_kg=price_per_kg,
+
+            state=state,
+
+            district=district,
+
+            subdistrict=subdistrict,
+
+            address=address,
+
+            latitude=latitude,
+
+            longitude=longitude,
+
+            main_contact_number=main_contact_number,
+
+            additional_contact_number=additional_contact_number
+        )
+
+
+        return redirect("admin_home")
+
+
+    return render(
+        request,
+        "admin_templates/storage_register.html",{"states":states}
+    )
